c04272b1d0980c4ce671c1837f9f3cd22293b889
GLayton-TX <74160349+GLayton-TX@users.noreply.github.com>
2022-06-17 18:28:10 -0500

    Initial Commit
    Initial Commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,25 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World!");
+//Controller
+function getString(){
+    let userString = document.getElementById("userString").value;
+    displayString(reverseString(userString));
+}
+
+//reverse the input string
+function reverseString(origString){
+    let reversedString = [];
+    for (i=origString.length; i>0; i--) {
+        reversedString += origString[i-1];
+    };
+    return reversedString;
+}
+
+//display the reversed string
+function displayString(reversedString){
+    document.getElementById("message").innerHTML = `Your reversed string is: <b>${reversedString}</b>`;
+    document.getElementById("alert").classList.remove("invisible");
+}
+
+// clear form
+function clearForm(){
+    location.reload();
 }
